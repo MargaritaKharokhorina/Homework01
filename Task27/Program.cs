28b64f7a9faf6313a8a3285b521b398c1b18de47
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2022-11-23 23:36:40 +0400

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27. Программа, которая принимает на вход число 
+// и выдает сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+if (number < 0) number = - number;
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма цифр числа {number} ---> {sumDigits}");
