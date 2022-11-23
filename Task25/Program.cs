24875f387f5e6455232e46059d32cc6716896a57
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2022-11-23 20:34:09 +0400

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25. Цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите через запятую два числа A и B");
+string[] tokens = Console.ReadLine()!.Split(',');
+int a = int.Parse(tokens[0]);
+int b = int.Parse(tokens[1]);
+string text = tokens[0].Trim();
+
+int Exponentiation(int numA, int numB)
+{
+    int result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = numA * result;
+    }
+    return result;
+}
+if (b > 0)
+{
+int exponentiation = Exponentiation(a, b);
+Console.WriteLine($"{a} в степени {b} --> {exponentiation}");
+}
+else Console.WriteLine("Второе число должно быть натуральным");
