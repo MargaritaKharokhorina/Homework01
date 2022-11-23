4e40f5341677a1737781d4da3f6f0237e4d0a6e4
Margarita Kharokhorina <kharokhorina_mb@mail.ru>
2022-11-24 00:30:39 +0400

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29. Программа, которая задает массив из 8 элементов,
+// заполненный псевдослучайными числами, и выводит их на экран.
+
+int[] array = new int[8];
+
+void RandomArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
+
+
